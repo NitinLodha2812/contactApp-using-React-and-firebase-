e36ad5b5ea8f29aca49462160e17febb50fc8d14
NitinLodha2812 <65410553+NitinLodha2812@users.noreply.github.com>
2020-06-19 10:49:20 +0530

    initial Commit

---
+++ src/utils/config.js
@@ -0,0 +1,19 @@
+//TODO: add firebase configuration and export it
+export const firebaseConfig = {
+  apiKey: "AIzaSyAvBLN5oJJvfkwNg7vI6kWb2VFfkU6El58",
+  authDomain: "mygitapp-9e6c5.firebaseapp.com",
+  databaseURL: "https://mygitapp-9e6c5.firebaseio.com",
+  projectId: "mygitapp-9e6c5",
+  storageBucket: "mygitapp-9e6c5.appspot.com",
+  messagingSenderId: "175943428504",
+  appId: "1:175943428504:web:c4ea3d7dbf75af802063ed",
+  measurementId: "G-8HFSK55WH1",
+};
+
+//image configuration
+export const imageConfig = {
+  quality: 0.2,
+  maxWidth: 800,
+  maxHeight: 600,
+  autoRotate: true,
+};
