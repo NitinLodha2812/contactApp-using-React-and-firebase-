b803d94cdb03117c26707acead9da21c92b5b4ca
Nitin Lodha <65410553+NitinLodha2812@users.noreply.github.com>
2020-06-19 12:16:47 +0530

    Update config.js

--- src/utils/config.js
+++ src/utils/config.js
@@ -1,13 +1,6 @@
 //TODO: add firebase configuration and export it
 export const firebaseConfig = {
-  apiKey: "AIzaSyAvBLN5oJJvfkwNg7vI6kWb2VFfkU6El58",
-  authDomain: "mygitapp-9e6c5.firebaseapp.com",
-  databaseURL: "https://mygitapp-9e6c5.firebaseio.com",
-  projectId: "mygitapp-9e6c5",
-  storageBucket: "mygitapp-9e6c5.appspot.com",
-  messagingSenderId: "175943428504",
-  appId: "1:175943428504:web:c4ea3d7dbf75af802063ed",
-  measurementId: "G-8HFSK55WH1",
+  //write your api config of firebase
 };
 
 //image configuration
